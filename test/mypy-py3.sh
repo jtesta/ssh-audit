ca6cfb81a2936e1eb3a5bb2ca381c5d446322a40
Andris Raugulis <moo@arthepsy.eu>
2016-10-19 20:51:57 +0300

    Import mypy configuration script and run scripts (for Python 2.7 and 3.5). Import pytest coverage script.

---
+++ test/mypy-py3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+_cdir=$(cd -- "$(dirname "$0")" && pwd)
+type mypy > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "err: mypy (Optional Static Typing for Python) not found."
+	exit 1
+fi
+_htmldir="${_cdir}/../html/mypy-py3"
+mkdir -p "${_htmldir}"
+mypy --python-version 3.5 --config-file "${_cdir}/mypy.ini" --html-report "${_htmldir}" "${_cdir}/../ssh-audit.py"
